301c9ca8988275559996af55e2a2ecc6eb6595fb
Dongpeng Xu <dongpengxu@psu.edu>
2015-10-28 15:10:45 -0400

    debug

--- lib/Transforms/Obfuscation/Dop.cpp
+++ lib/Transforms/Obfuscation/Dop.cpp
@@ -16,8 +16,8 @@ namespace {
 
         bool runOnFunction(Function &F) override {
             if(toObfuscate(flag,&F,"dop")) {
-                StringRef sr = new StringRef("fun");
-                if (F.getName().equals(sr)) {
+                StringRef *sr = new StringRef("fun");
+                if (F.getName().equals(*sr)) {
                     errs() << "Hello: ";
                     errs().write_escaped(F.getName()) << '\n';
                     
