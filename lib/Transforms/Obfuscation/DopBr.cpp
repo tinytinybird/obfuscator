bd724bfd5c189722cb2087ef72e49f3153b1eb41
Dongpeng Xu <dongpengxu@psu.edu>
2015-11-08 12:24:19 -0500

    debug

--- lib/Transforms/Obfuscation/DopBr.cpp
+++ lib/Transforms/Obfuscation/DopBr.cpp
@@ -42,66 +42,70 @@ namespace {
         }
 
         // build dependence
-        void builddep(Instruction *iuse, std::set<Instruction*> *idep) {
-            for (User::op_iterator opi = iuse->op_begin(), ope = iuse->op_end(); opi != ope; ++opi) {
-                Instruction *vi = dyn_cast<Instruction>(*opi);
-                if (vi == NULL) return;
-                else {
-                    switch (vi->getOpcode()) {
-                    case Instruction::Add:
-                        idep->insert(vi);
-                        builddep(dyn_cast<Instruction>(*vi->getOperand(0)), idep);
-                        builddep(dyn_cast<Instruction>(*vi->getOperand(1)), idep);
-                        return;
-                    case Instruction::Sub:
-                        idep->insert(vi);
-                        builddep(dyn_cast<Instruction>(*vi->getOperand(0)), idep);
-                        builddep(dyn_cast<Instruction>(*vi->getOperand(1)), idep);
-                        return;
-                    case Instruction::Mul:
-                        idep->insert(vi);
-                        builddep(dyn_cast<Instruction>(*vi->getOperand(0)), idep);
-                        builddep(dyn_cast<Instruction>(*vi->getOperand(1)), idep);
-                        return;
-                    case Instruction::Br:
-                        idep->insert(vi);
-                        builddep(dyn_cast<Instruction>(*vi->getOperand(0)), idep);
-                        return;
-                    case Instruction::Load:
-                        idep->insert(vi);
-                        BasicBlock::iterator i = vi;
-                        for (BasicBlock::iterator j = vi->getParent()->end(); i != j; --i) {
-                            if (i->getOpcode() == Instruction::Store && i->getOperand(1) == vi->getOperand(0)) {
-                                break;
-                            }
-                        }
-                        builddep(dyn_cast<Instruction>(*i), idep);
-                        return;
-                    case Instruction::Store:
-                        idep->insert(vi);
-                        builddep(dyn_cast<Instruction>(*vi->getOperand(1)), idep);
-                        return;
-                    case Instruction::Alloca:
-                        idep->insert(vi);
-                        return;
-                    case Instruction::ICmp:
-                        idep->insert(vi);
-                        builddep(dyn_cast<Instruction>(*vi->getOperand(0)), idep);
-                        return;
+    private:
+        void builddep(Instruction *iuse, std::set<Instruction*> *idep);
+    };
+}
+
+
+void DopBr::builddep(Instruction *iuse, std::set<Instruction*> *idep) {
+    for (User::op_iterator opi = iuse->op_begin(), ope = iuse->op_end(); opi != ope; ++opi) {
+        Instruction *vi = dyn_cast<Instruction>(*opi);
+        if (vi == NULL) return;
+        else {
+            switch (vi->getOpcode()) {
+            case Instruction::Add:
+                idep->insert(vi);
+                builddep(dyn_cast<Instruction>(*vi->getOperand(0)), idep);
+                builddep(dyn_cast<Instruction>(*vi->getOperand(1)), idep);
+                return;
+            case Instruction::Sub:
+                idep->insert(vi);
+                builddep(dyn_cast<Instruction>(*vi->getOperand(0)), idep);
+                builddep(dyn_cast<Instruction>(*vi->getOperand(1)), idep);
+                return;
+            case Instruction::Mul:
+                idep->insert(vi);
+                builddep(dyn_cast<Instruction>(*vi->getOperand(0)), idep);
+                builddep(dyn_cast<Instruction>(*vi->getOperand(1)), idep);
+                return;
+            case Instruction::Br:
+                idep->insert(vi);
+                builddep(dyn_cast<Instruction>(*vi->getOperand(0)), idep);
+                return;
+            case Instruction::Load:
+                idep->insert(vi);
+                BasicBlock::iterator i = vi;
+                for (BasicBlock::iterator j = vi->getParent()->end(); i != j; --i) {
+                    if (i->getOpcode() == Instruction::Store && i->getOperand(1) == vi->getOperand(0)) {
+                        break;
                     }
                 }
+                builddep(dyn_cast<Instruction>(*i), idep);
+                return;
+            case Instruction::Store:
+                idep->insert(vi);
+                builddep(dyn_cast<Instruction>(*vi->getOperand(1)), idep);
+                return;
+            case Instruction::Alloca:
+                idep->insert(vi);
+                return;
+            case Instruction::ICmp:
+                idep->insert(vi);
+                builddep(dyn_cast<Instruction>(*vi->getOperand(0)), idep);
+                return;
             }
         }
-    };
+    }
 }
 
 char DopBr::ID = 0;
 static RegisterPass<DopBr> X("DopBr", "Dynamic opaque predicate obfuscation Pass for straight line code");
 
 Pass *llvm::createDopBr() {
-  return new DopBr();
+    return new DopBr();
 }
 
 Pass *llvm::createDopBr(bool flag) {
-  return new DopBr(flag);
+    return new DopBr(flag);
 }
