b586472c7ad1f4e7c47156bc69dc2cf79836a4b3
Dongpeng Xu <dongpengxu@psu.edu>
2015-11-12 15:35:22 -0500

    add function isloop

--- lib/Transforms/Obfuscation/DopLoop.cpp
+++ lib/Transforms/Obfuscation/DopLoop.cpp
@@ -1,6 +1,7 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/CFG.h"
 
 #include "llvm/Transforms/Obfuscation/DopLoop.h"
 #include "llvm/Transforms/Obfuscation/Utils.h"
@@ -25,9 +26,52 @@ namespace {
             return false;
         }
         void addDopLoop(Function &F) {
-            errs() << "Set up files for DopLoop" << "\n";
+
+            for (Function::iterator bb = F.begin(), e = F.end(); bb != e; ++bb) {
+                if (isloop(bb)) {
+                    errs() << "find a loop in BB" << "\n";
+                } else {
+                    errs() << "not a loop" << "\n";
+                }
+            }
+
+            // insert allca for the dop pointers
+            // BasicBlock::iterator ii = std::next(insertAlloca);
+            // AllocaInst* dop1 = new AllocaInst(Type::getInt32PtrTy(F.getContext()), 0, 4, "dop1");
+            // AllocaInst* dop2 = new AllocaInst(Type::getInt32PtrTy(F.getContext()), 0, 4, "dop2");
+            // AllocaInst* dop1br1 = new AllocaInst(Type::getInt32PtrTy(F.getContext()), 0, 4, "dop1br1");
+            // AllocaInst* dop2br1 = new AllocaInst(Type::getInt32PtrTy(F.getContext()), 0, 4, "dop2br1");
+            // AllocaInst* dop1br2 = new AllocaInst(Type::getInt32PtrTy(F.getContext()), 0, 4, "dop1br2");
+            // AllocaInst* dop2br2 = new AllocaInst(Type::getInt32PtrTy(F.getContext()), 0, 4, "dop2br2");
+
         }
+
+        bool isloop(BasicBlock *bb);
     };
+}
+
+// test wether a basic block is a loop
+bool DopLoop::isloop(BasicBlock *bb)
+{
+    Instruction *bbend = bb->getTerminator();
+    BranchInst *br = dyn_cast<BranchInst>(bbend);
+
+    // skip the unconditional jumps
+    while (br && br->isUnconditonal()) {
+        Instruction *next = br->getSuccessor()->getTerminator();
+        BranchInst *nextbr = dyn_cast<BranchInst>(next);
+        br = nextbr ? nextbr : NULL;
+    }
+
+    if (br) {
+        BasicBlock *BB = br->getParent;
+        for (succ_iterator PI = succ_begin(BB), E = succ_end(BB); PI != E; ++PI) {
+            if (*PI == bb)
+                return true;
+        }
+        return false;
+    } else
+        return false;
 }
 
 char DopLoop::ID = 0;
